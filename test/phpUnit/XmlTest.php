db34432a2ece762d61a8cf77016650e0918e9da4
Dominic Jodoin <dominic@travis-ci.com>
2017-05-16 15:46:18 -0400

    Don't pass an empty string

--- test/phpUnit/XmlTest.php
+++ test/phpUnit/XmlTest.php
@@ -205,7 +205,6 @@ class XmlTest extends PHPUnit_Framework_TestCase
         $a("//TARGET")->before("<before/>")->after($a->doc->createElement("after"))->append("<append/>")->prepend("<prepend/>");
         $this->assertEquals($a("/root")->xml(), "<root><before></before><TARGET><prepend></prepend><append></append></TARGET><after></after></root>", "failed");
 
-        $c = "";
         $a = new Xms\Core\Xml($c);
         $c = "<r><EXTERNAL/>TEXT<?PI some instructions?><CONTENT/></r>";
         $b = new Xms\Core\Xml($c);
